cf0b5c8d35746743a914101fed4014163d019736
Abdullah Alshalan <a.shalan443@gmail.com>
2025-11-15 05:45:59 +0300

    completed BDD tests

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -132,3 +132,26 @@ def step_impl(context, element_name, text_string):
     )
     element.clear()
     element.send_keys(text_string)
+
+@when('I press the "{element_name}" button')
+def step_impl(context, element_name):
+    element_id = element_name.lower() + "-btn"
+    element = context.driver.find_element(By.ID, element_id)
+    element.click()
+
+
+@then('I should see the message "{msg}"')
+def step_impl(context, msg):
+    flash_message = context.driver.find_element(By.ID, "flash_message")
+    assert msg in flash_message.text
+
+
+@then('I should see "{search_text}" in the results')
+def step_impl(context, search_text):
+    result = context.driver.find_element(By.ID, "search_results")
+    assert search_text in result.text
+
+@then('I should not see "{search_text}" in the results')
+def step_impl(context, search_text):
+    result = context.driver.find_element(By.ID, "search_results")
+    assert search_text not in result.text
