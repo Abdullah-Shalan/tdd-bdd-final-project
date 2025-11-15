5db8b7abe226b03334689f50bfcfbfcc30e32fff
Abdullah Alshalan <a.shalan443@gmail.com>
2025-11-15 04:03:09 +0300

    implemented factory product

--- tests/factories.py
+++ tests/factories.py
@@ -21,6 +21,7 @@ import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
+PRODUCT_NAMES = ["Hat", "Pants", "Shirt", "Apple", "Banana", "Pots", "Towels", "Ford", "Chevy", "Hammer", "Wrench"]
 
 class ProductFactory(factory.Factory):
     """Creates fake products for testing"""
@@ -31,4 +32,15 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+    name = FuzzyChoice(choices=PRODUCT_NAMES)
+    description = factory.Faker("text")
+    price = FuzzyDecimal(0.5, 200, 2)
+    available = FuzzyChoice(choices=[True, False])
+    category = FuzzyChoice(choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE,
+            Category.TOOLS,
+        ])
